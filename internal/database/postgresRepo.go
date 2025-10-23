6ca340efa9cce3b8038d26983e817af5f51e5ba5
The Hoang <phanthehoang2503@gmail.com>
2025-10-23 13:07:59 +0700

    major update on product service, fix connection string (host used to be postgres but without docker its got error. changed to localhost), add auto migrate (without it will cause error)

--- internal/database/postgresRepo.go
+++ internal/database/postgresRepo.go
@@ -1,18 +1,22 @@
-package config
+package database
 
 import (
 	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectDB() (*gorm.DB, error) {
+	godotenv.Load()
+
 	dsn := os.Getenv("POSTGRES_DSN")
 	if dsn == "" {
 		dsn = "host=postgres user=postgres password=25032004 dbname=mydb port=5432 sslmode=disable"
 	}
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("open db: %w", err)
--- product-service/cmd/product-service/main.go
+++ product-service/cmd/product-service/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/phanthehoang2503/small-project/internal/database"
+	"github.com/phanthehoang2503/small-project/product-service/internal/model"
 	"github.com/phanthehoang2503/small-project/product-service/internal/repo"
 	"github.com/phanthehoang2503/small-project/product-service/internal/router"
 )
 
 func main() {
-	r := gin.Default()
-	repo := repo.NewRepo()
+	db, err := database.ConnectDB() //connect to db
+	if err != nil {
+		log.Fatal("failed to connect to database...")
+	}
+	productRepo := repo.NewRepo(db) //initial repository
+	if err := db.AutoMigrate(&model.Product{}); err != nil {
+		log.Fatalf("Migration failed: %v", err)
+	}
 
-	router.RegisterRoutes(r, repo)
+	r := gin.Default()
+	router.RegisterRoutes(r, productRepo)
 	r.Run(":8080")
 }
