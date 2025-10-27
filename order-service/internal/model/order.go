466c088592d920726068ce21afd1e5932c3fc11b
The Hoang <phanthehoang2503@gmail.com>
2025-10-27 09:04:12 +0700

    completed order model

--- order-service/internal/model/order.go
+++ order-service/internal/model/order.go
@@ -7,5 +7,14 @@ type Order struct {
 	UserId uint   `json:"user_id"`
 	Total  int64  `json:"total"`
 	Status string `json:"status"`
-	Item[]
+	Item   []OrderItem
+}
+
+type OrderItem struct {
+	gorm.Model
+	OrderId   uint  `json:"order_id"`
+	ProductId uint  `json:"product_id"`
+	Quantity  int   `json:"quantity"`
+	Price     int64 `json:"price"`
+	Subtotal  int64 `json:"subtotal"`
 }
